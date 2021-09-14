54cbded516a1a89228e41c9037526a3dd519784e
Elber Nava <enava@jacidi.com>
2021-09-14 15:50:50 -0400

    separate index.scss
    created component and responsive modules to separate the structure of the index.scss file

--- js/index.js
+++ js/index.js
@@ -1,40 +1,20 @@
-function showContactInfo() {
-  cuteAlert({
-    type: 'success',
-    title: 'Contáctame',
-    message: `Este es mi número de teléfono <br/> <strong>+58 414-4964508</strong><br/><br/>
-        Puedes enviarme un correo a esta dirección <br/> <strong>elbernava11@gmail.com</strong>`,
-    buttonText: 'Ok',
-    icon: 'email-plus-outline',
-  });
-}
-
-function showWhatsapp() {
-  cuteAlert({
-    type: 'success',
-    title: 'Whatsapp',
-    message: `Este es mi número de teléfono <br/> <strong>+58 414-4964508</strong>`,
-    buttonText: 'Ok',
-    icon: 'whatsapp',
-  });
-}
-
-function showEmail() {
-  cuteAlert({
-    type: 'success',
-    title: 'Corre Eletrónico',
-    message: `Puedes enviarme un correo a esta dirección <br/> <strong>elbernava11@gmail.com</strong>`,
-    buttonText: 'Ok',
-    icon: 'gmail',
-  });
-}
-
 function goToGitlab() {
   window.open('https://gitlab.com/users/Elber-17/projects', '_blank');
 }
 
 function goToLinkedIn() {
   window.open('https://www.linkedin.com/in/elber-nava-9147aa186/', '_blank');
+}
+
+const drawShell = () => {
+  let shell = new Shell('#awesome-shell', {
+    user: 'elber',
+    host: 'dev',
+    path: '~',
+    style: 'ubuntu',
+    commands: ['First command', 'Second command', '...'],
+    typed: Typed
+  });
 }
 
 AOS.init({ duration: 850 });
@@ -46,13 +26,5 @@ new Vivus(
 );
 
 document.addEventListener('DOMContentLoaded', function () {
-  let shell = new Shell('#awesome-shell', {
-    user: 'elber',
-    host: 'ubuntu',
-    path: '~',
-    style: 'ubuntu',
-    theme: 'dark',
-    responsive: false,
-    commands: ['First command', 'Second command', '...'],
-  });
+  drawShell()
 });
