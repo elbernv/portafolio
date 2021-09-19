be4aa1df986b60c96a87f4bfb9c977e032d1a957
Elber <elber.nava17@gmail.com>
2021-09-19 18:10:43 -0400

    Background fixed, updaded lib AOS. updated me picture.

---
+++ js/canvas.js
@@ -0,0 +1,85 @@
+var space;
+
+function floatySpace() {
+  var colors = ['#FF3F8E', '#04C2C9', '#2E55C1'];
+
+  space = new CanvasSpace('canvas', '#252934').display();
+  let form = new Form(space);
+
+  // Elements
+  let pts = [];
+  let center = space.size.$divide(1.8);
+  let angle = -(window.innerWidth * 0.5);
+  let count = window.innerWidth * 0.05;
+  if (count > 150) count = 150;
+  let line = new Line(0, angle).to(space.size.x, 0);
+  let mouse = center.clone();
+
+  let r = Math.min(space.size.x, space.size.y) * 1;
+  for (let i = 0; i < count; i++) {
+    let p = new Vector(
+      Math.random() * r - Math.random() * r,
+      Math.random() * r - Math.random() * r
+    );
+    p.moveBy(center).rotate2D((i * Math.PI) / count, center);
+    p.brightness = 0.1;
+    pts.push(p);
+  }
+
+  // Canvas
+  space.add({
+    animate: function (time, fps, context) {
+      for (let i = 0; i < pts.length; i++) {
+        // rotate the points slowly
+        let pt = pts[i];
+
+        pt.rotate2D(Const.one_degree / 20, center);
+        form
+          .stroke(false)
+          .fill(colors[i % 3])
+          .point(pt, 1);
+
+        // get line from pt to the mouse line
+        let ln = new Line(pt).to(line.getPerpendicularFromPoint(pt));
+
+        // opacity of line derived from distance to the line
+        let opacity = Math.min(
+          0.8,
+          1 - Math.abs(line.getDistanceFromPoint(pt)) / r
+        );
+        let distFromMouse = Math.abs(ln.getDistanceFromPoint(mouse));
+
+        if (distFromMouse < 50) {
+          if (pts[i].brightness < 0.3) pts[i].brightness += 0.015;
+        } else {
+          if (pts[i].brightness > 0.1) pts[i].brightness -= 0.01;
+        }
+
+        let color = 'rgba(255,255,255,' + pts[i].brightness + ')';
+        form.stroke(color).fill(true).line(ln);
+      }
+    },
+
+    onMouseAction: function (type, x, y, evt) {
+      if (type == 'move') {
+        // console.log(mouse.x);
+        mouse.set(x, y);
+      }
+    },
+
+    onTouchAction: function (type, x, y, evt) {
+      this.onMouseAction(type, x, y);
+    },
+  });
+
+  space.bindMouse();
+  space.play();
+}
+
+floatySpace();
+
+window.onresize = () => {
+  space.removeAll();
+  document.getElementById('canvas').remove();
+  floatySpace();
+};
--- js/index.js
+++ js/index.js
@@ -1,30 +1,43 @@
-function goToGitlab() {
-  window.open('https://gitlab.com/users/Elber-17/projects', '_blank');
-}
-
-function goToLinkedIn() {
-  window.open('https://www.linkedin.com/in/elber-nava-9147aa186/', '_blank');
-}
-
-const drawShell = () => {
-  let shell = new Shell('#awesome-shell', {
-    user: 'elber',
-    host: 'dev',
-    path: '~',
-    style: 'ubuntu',
-    commands: ['First command', 'Second command', '...'],
-    typed: Typed
+const typedTechnologies = () => {
+  new Typed('#technologies', {
+    strings: ['Python', 'Node JS', 'Vue JS', 'Angular JS'],
+    typeSpeed: 150,
+    backSpeed: 130,
+    loop: true,
+    backDelay: 1200,
   });
-}
+};
 
-AOS.init({ duration: 850 });
+const setContainersMaxWith = (containers) => {
+  let screenWidth = window.screen.width;
 
-new Vivus(
-  'svg-devices',
-  { duration: 300, file: './img/vectorpaint.svg' },
-  () => {}
-);
+  if (Array.isArray(containers)) {
+    for (let container of containers) {
+      container.style.width =
+        screenWidth - Math.ceil((screenWidth * 1.0466) / 100) + 'px';
+    }
+
+    return;
+  }
+
+  containers.style.width =
+    screenWidth - Math.ceil((screenWidth * 1.0466) / 100) + 'px';
+
+  space.removeAll();
+  document.getElementById('canvas').remove();
+  floatySpace();
+
+  return;
+};
 
 document.addEventListener('DOMContentLoaded', function () {
-  drawShell()
+  AOS.init({ duration: 800 });
+
+  let containersRequireMaximumWidth = Array.from(
+    document.getElementsByClassName('max-width')
+  );
+  let canvasBackground = document.getElementById('pt');
+  typedTechnologies();
+  setContainersMaxWith(containersRequireMaximumWidth);
+  setContainersMaxWith(canvasBackground);
 });
